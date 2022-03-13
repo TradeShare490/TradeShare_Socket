41c8034e85223245e63837f88be0ce92bc211818
Tung Leu <leudeptrai@gmail.com>
2022-03-13 06:21:46 -0400

    edited server

--- src/app.ts
+++ src/app.ts
@@ -4,59 +4,58 @@ import { Server } from "socket.io";
 
 const port = process.env.PORT || 6969;
 const host = process.env.HOST || "http://localhost";
-const corsOrigin = "*";
 const app = express();
 
 const httpServer = createServer(app);
-
-const io = new Server(httpServer, {
-	cors: {
-		origin: corsOrigin,
-		credentials: true,
-	},
-});
-
-let users: any[] = [];
+let users: any = [];
 
 const addUser = (userId: any, socketId: any) => {
-	!users.some((user) => user.userId === userId) && users.push({ userId, socketId });
+	!users.some((user: { userId: any, socketId: any; }) => user.userId === userId) &&
+		users.push({ userId, socketId });
 };
 
 const removeUser = (socketId: any) => {
-	users = users.filter((user) => user.socketId !== socketId);
+	users = users.filter((user: { userId: any, socketId: any; }) => user.socketId !== socketId);
 };
 
 const getUser = (userId: any) => {
-	return users.find((user) => user.userId === userId);
+	return users.find((user: { userId: any, socketId: any; }) => user.userId === userId);
 };
+const io = new Server(httpServer, {
+	cors: {
+		origin: 'http://localhost:8081',
+		methods: ["GET", "POST"],
+		credentials: true
+	},
+	allowEIO3: true,
+
+});
 
 io.on("connection", (socket) => {
-	console.log("a user connected.");
+	socket.on("addUser", (userId: any) => {
+		addUser(userId, socket.id);
+		io.emit("getUsers", users);
+	});
 
-    socket.on("addUser", (userId) => {
-        addUser(userId, socket.id);
-        io.emit("getUsers", users);
-    });
+	socket.on("sendMessage", ({ sender, receiver, content }: { sender: any, receiver: any, content: any }) => {
+		console.log(receiver)
+		const user = getUser(receiver);
+		console.log(user)
+		io.to(user.socketId).emit("getMessage", {
+			sender,
+			content
+		});
+	});
+	// fetch existing users
 
-    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-        const user = getUser(receiverId);
-        io.to(user.socketId).emit("getMessage", {
-          senderId,
-          text,
-        });
-      });
-    
-      //when disconnect
-      socket.on("disconnect", () => {
-        console.log("a user disconnected!");
-        removeUser(socket.id);
-        io.emit("getUsers", users);
-      });
-
+	socket.on("disconnect", () => {
+		removeUser(socket.id);
+		socket.broadcast.emit("user disconnected", socket.id);
+	});
 });
 app.get("/", (_, res) => res.send(`Server is up and running`));
 
-httpServer.listen(port,() => {
+httpServer.listen(port, () => {
 	console.log("Server is listening");
 	console.log(`${host}:${port}`);
 });
